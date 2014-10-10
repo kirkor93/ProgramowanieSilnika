1566ba1436403d75b76970245e4d339e0cbe474d
Krzysztof Szczech <krzysztof.szczech@hotmail.com>
2014-10-10 13:57:27 +0200

    DirectX initialized

--- Engine2D/Engine2D/GraphicsClass.h
+++ Engine2D/Engine2D/GraphicsClass.h
@@ -1,8 +1,26 @@
 #pragma once
+
+#include <windows.h>
+
+#include "D3DClass.h"
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
 class GraphicsClass
 {
 public:
 	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
 	~GraphicsClass();
-};
 
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+	D3DClass* m_D3D;
+};
